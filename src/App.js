bc741b7027be1568c98227007a46ccc6a0582832
Seung Hyun Kim <seung.kim0708@gmail.com>
2022-03-02 09:56:48 -0800

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import HomePage from './components/HomePage/Homepage';
+import './App.css';
+
+
+
+const App = () => {
+  return (
+    <div className="App">
+      <HomePage />
+    </div>
+  );
+}
+
+export default App;
